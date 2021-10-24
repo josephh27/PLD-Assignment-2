3ee8072d4ee1f34c47cddc9028d1ea26d59a7a6a
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2021-10-24 18:32:37 +0800

    Assignment#2

---
+++ Name__age__and__address_assignment.py
@@ -0,0 +1,4 @@
+a = input("Please enter your name summoner:")
+b = input("How many years have you dwelled summoner?")
+c = input("Where do you dwell summoner?")
+print(f"Hi, my name is {a}. I am {b} years old and I live in {c}.")
