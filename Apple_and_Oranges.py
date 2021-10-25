3fd64613f832325bafafeb5c50bf25d51d546ddc
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2021-10-25 12:19:51 +0800

    Assignment pt.2

---
+++ Apple_and_Oranges.py
@@ -0,0 +1,6 @@
+a = int(input("How many apples would you like to buy? It is 20 PHP per apple:"))
+b = int(input("How many oranges would you like to buy? It is 25 PHP per orange:"))
+result = a*20 + b*25
+print(f"{a}(20PHP) + {b}(25PHP) = {result}")
+print(f"The total amount is {result} PHP")
+print("Thank you for shopping!")
