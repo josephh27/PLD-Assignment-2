2ffe6012274ce21647105209e6a17b0fdd33a405
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2021-10-25 12:57:43 +0800

    part 3

---
+++ Assignment_3_Part_3.py
@@ -0,0 +1,6 @@
+y = float(input("What is the price of a single apple? "))
+a = float(input("How much money do you have? "))
+x = a//y
+b = a % y
+print(f"You can buy {x} apple(s) and your change is {b} PHP")
+print("HAPPY TO SERVE")
